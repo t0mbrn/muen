7964d1a893f609e08c3bbaa9927679e4162a3abe
Reto Buerki <reet@codelabs.ch>
2017-07-07 17:32:37 +0200

    DEBUG Enable turbo mode on all cores
    Code is an illustration on how to control processor frequencies via
    IA32_PERF_CTL/IA32_PERF_STATUS MSRs. It assumes that SpeedStep is
    active, otherwise the system will reset when attempting to write
    IA32_PERF_CTL.
    
    Findings:
    
    * Software disengage (bit 32 in IA32_PERF_CTL) does not prevent
      frequency changes on AC unplug/plug events.
    
    * Also setting IA32_MISC_ENABLE bit 38 (which is intended for BIOS
      according to Intel SDM Vol. 3A, 14.3.2.1) has only a temporary effect
      if SpeedStep is enabled in the BIOS.
    
    * Only disabling SpeedStep in the BIOS had the effect that AC events did
      not affect CPU frequencies.
    
    Tested on Lenovo T440s.

--- kernel/src/sk-kernel.adb
+++ kernel/src/sk-kernel.adb
@@ -36,6 +36,7 @@ with SK.VTd.Debug;
 with SK.VTd.Interrupts;
 with SK.Interrupts;
 with SK.Crash_Audit_Types;
+with SK.Power;
 
 package body SK.Kernel
 is
@@ -1070,6 +1071,8 @@ is
          FPU.Enable;
          Apic.Enable;
          MCE.Enable;
+
+         Power.Turbo;
 
          if CPU_Info.Is_BSP then
             --D @Item List => impl_kernel_init_steps
